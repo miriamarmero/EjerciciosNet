afb6e0a26a3e7bd8033283474a701cc3f7a32362
formacion <formacion@gtt.es>
2019-07-04 14:53:57 +0200

    añadido de Ejercicios Bucles, En Proceso...

---
+++ EjerciciosBucles/1-PrimoONo/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+//Dado un numero introducido por el usuario devovler si es primo o no.
+
+namespace _1_PrimoONo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
