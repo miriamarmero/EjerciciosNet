513f4f551bebf0f6a0fe6206c558fb4338cb1946
formacion <formacion@gtt.es>
2019-07-05 14:54:16 +0200

    añadido de get, set, patch, delete

--- ApuntesClase/TodoApi/Controllers/ToDoController.cs
+++ ApuntesClase/TodoApi/Controllers/ToDoController.cs
@@ -1,6 +1,4 @@
-using System;
 using System.Collections.Generic;
-using Microsoft.EntityFrameworkCore;
 using TodoApi.Models;
 using Microsoft.AspNetCore.Mvc;
 using TodoApi.Utils;
@@ -27,6 +25,42 @@ namespace TodoApi.Controllers
             this.store.todos.Add(item);
             item.Id = this.store.todos.Count;
             return item;
+        }
+
+        [HttpGet("{id}")]
+        public TodoItem GetTodoItems(long id){
+            return this.store.todos.Find(todo => {
+                System.Console.WriteLine("todo: " + todo.Id);
+                return todo.Id == id;
+            });
+        }
+
+        [HttpGet("busqueda/{input}")]
+        public List<TodoItem> GetTodoItems(string input){
+            return this.store.todos.FindAll(todo => {
+                return todo.Name.Contains(input);
+            });
+        }
+
+        [HttpGet("done")]
+        public List<TodoItem> GetTodoIsCompleted(){
+            return this.store.todos.FindAll(todo => {
+                return todo.IsComplete == true;
+            });
+        }
+
+        [HttpPatch("done/{id}")]
+        public TodoItem PatchTodoIsCompleted(long id){
+            int posicion = this.store.todos.FindIndex(todo => todo.Id == id);
+            this.store.todos[posicion].IsComplete = true;
+            return this.store.todos[posicion];
+        }
+
+        [HttpDelete("{id}")]
+        public TodoItem DeleteTodoId(long id){
+            return this.store.todos.Find(todo => {
+                return todo.Id == id;
+            });
         }
     }
 }
